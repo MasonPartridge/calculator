04dedc0cf2299b4b1255b4747663e27291c88c3f
MasonPartridge <masonepartridge@gmail.com>
2023-07-03 12:13:40 -0700

    Implement logic for calculator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './../public/style.css'
 
 function App() {
 
-    const [operationString, setOperationString] = useState('');
+    const [operations, setOperations] = useState([]);
 
     function add(a, b) {
         return a + b;
@@ -28,37 +28,82 @@ function App() {
 
     function assembleKeyPad() {
         var arr = []
-        for (var i = 1; i <= 3; i++) {
+        for (var i = 0; i < 3; i++) {
             var arr2 = []
             for (var i2 = 0; i2 < 3 * 1; i2++) {
-                arr2.push(i2 + i * 3);
+                arr2.push(i2 + i * 3 + 1);
             }
             arr.push(arr2);
         }
         return arr;
     }
 
+    function addOperationToOperations(inputOperation) {
+        if (Number.isInteger(inputOperation)) {
+            if (Number.isInteger(operations[operations.length - 1])) {
+                setOperations(operations.map((operation, index) => {
+                    if (index === operations.length - 1) {
+                        return operation * 10 + inputOperation;
+                    }
+                }))
+                return;
+            }
+            setOperations(operations.concat(inputOperation));
+        }
+        if (operations.length != 0) {
+            setOperations(operations.concat(inputOperation));
+        }
+    }
+
+    useEffect(() => {
+        console.log(operations);
+    }, [operations])
+
+    function operate(arr) {
+        var isAddition = true;
+        return arr.reduce((output, operation) => {
+            if (Number.isInteger(operation)) {
+                return (isAddition) ? output + operation : output - operation;
+            }
+            switch (operation) {
+                case '+':
+                    isAddition = true;
+                    break;
+                case '-':
+                    isAddition = false;
+                    break;
+            }
+            return output;
+        }
+            , 0)
+    }
+
     return (
         <div className='flex flex-col items-center justify-center'>
             <div>
-                <input className='border-black border-2 bg-slate-500 m-4' type='text'></input>
+                <input
+                    className='border-black border-2 bg-slate-500 m-4'
+                    type='text' readOnly value={
+                        operations.reduce(
+                            (output, operator) => output + operator, '') 
+                            + '=' + operate(operations)
+                    }></input>
             </div>
             <div className='flex flex-row'>
                 <div>
                     {assembleKeyPad().map((row) =>
                         <div key={row[0]} className='flex flex-row'>
                             {row.map((num) =>
-                                <button className='w-8 m-1 rounded-xl bg-gray-600' 
-                                    key={num} onClick={() => {
-                                    setOperationString(operationString + num)
-                                }}>{num}</button>
+                                <button className='w-8 m-1 rounded-xl bg-gray-600'
+                                    key={num} onClick={() => { addOperationToOperations(num) }}>{num}</button>
                             )}
                         </div>
                     )}
                 </div>
                 <div className='flex flex-col'>
-                    {['+', '-', '='].map((operator) =>
-                        <button key={operator} 
+                    {['+', '-', 0].map((operator) =>
+                        <button onClick={() => { addOperationToOperations(operator) }}
+                            key={operator}
                             className='w-8 m-1 rounded-xl bg-gray-600'
                         >{operator}</button>
                     )}
